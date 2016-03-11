4d4ea3e04c0780bedb11a5bbd8a87a89602c5398
Arjhun Srinivas <arjhun.s@gmail.com>
2016-03-11 12:52:08 +0530

    Added clusters endpoint

--- server.js
+++ server.js
@@ -13,12 +13,12 @@ app.use(bodyParser.urlencoded({ extended : false}));
 app.use(bodyParser.json());
 
 var connection = mysql.createConnection({
-  host : 'localhost',
-  user : 'root',
+  host : 'librorum.in',
+  user : 'arjhun',
   password : '$haringan1208!',
   database : 'c9'
 });
-/*
+
 if(!connection.connect()) {
     console.log('Database offline');
 }
@@ -42,7 +42,7 @@ app.get('/users', function(req,res) {
     res.send('UNIQUE exception');
   });
 });
-*/
+
 app.post('/upload', function(req,res) {
   var form = new multiparty.Form();
   dir = req.headers.user_id;
@@ -56,7 +56,7 @@ app.post('/upload', function(req,res) {
     if(typeof files !== 'undefined' && files) {
       // Generate new path for image
       newPath = './' + dir + '/' + files.upload[0].originalFilename;
-      
+
       // Move file from tmp to local project
       if(!fs.existsSync(dir)) {
         fs.mkdirSync(dir,0766);
@@ -64,10 +64,10 @@ app.post('/upload', function(req,res) {
       } else {
         fs.renameSync(files.upload[0].path, newPath);
       }
-      
+
       // Insert into database
       sqlString = 'INSERT INTO photos (user_id,Image_name,Image_path,local_path,Image_time_stamp) VALUES (?,?,?,?,?)';
-      
+
       // Populate values array
       values = [];
       values.push(dir);
@@ -76,13 +76,31 @@ app.post('/upload', function(req,res) {
       values.push(req.headers.path);
       values.push((moment(Number(req.headers.timestamp)).format('YYYY-MM-DD HH:MM:SS')));
       //values.push(req.headers.timestamp);
-      
+
       // Execute query
       connection.query(sqlString,values,function(err,result) {
         if(err) {
           console.log(err);
         }
       });
+    }
+  });
+});
+
+app.get('/clusters', function(req,res) {
+  // SQL query
+  sqlString = 'SELECT image_name,local_path FROM photos WHERE user_id = ?';
+
+  // Populate values array
+  values = [];
+  values.push(req.query.user_id);
+
+  connection.query(sqlString,values,function(err,result) {
+    if(err) {
+      console.log(err);
+      res.send('Error');
+    } else {
+      res.send(result);
     }
   });
 });
